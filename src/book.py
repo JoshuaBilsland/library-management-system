87da9a3773e4561b8cdc73a740a7a6a5aa1530cb
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-01-25 20:56:49 +0000

    feat: add book class

---
+++ src/book.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author):
+        self.__title = title
+        self.__author = author
+        self.__is_borrowed = False
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def is_borrowed(self):
+        return self.__is_borrowed
+
+    def set_is_borrowed(self, boolean):
+        self.__is_borrowed = boolean
