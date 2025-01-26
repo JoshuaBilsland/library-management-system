d3713c2d32bcdcac6f4e351bd335247b2c24b3fd
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-01-26 20:28:35 +0000

    fix: change member to user for consistency

--- src/library.py
+++ src/library.py
@@ -1,14 +1,14 @@
 class Library:
     def __init__(self):
         self.__books = []
-        self.__members = []
+        self.__users = []
         self.__staff = []
 
     def get_books(self):
         return self.__books
 
-    def get_members(self):
-        return self.__members
+    def get_users(self):
+        return self.__users
 
     def get_staff(self):
         return self.__staff
@@ -16,8 +16,8 @@ class Library:
     def add_book(self, book):
         self.__books.append(book)
 
-    def add_member(self, member):
-        self.__members.append(member)
+    def add_user(self, user):
+        self.__users.append(user)
 
     def add_staff(self, staff):
         self.__staff.append(staff)
